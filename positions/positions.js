d7bb241dbbdb1f237b3f7b7b22d0c38ad2117b6d
Antoan <lottex123@gmail.com>
2023-10-03 16:48:43 +0200

    Update code

--- positions/positions.js
+++ positions/positions.js
@@ -5,38 +5,38 @@ const { sendMessage } = require('../discord/functions');
 const { formatUserMessage } = require('../utils/global');
 
 var positions = [];
-const viableTickers = ['DOGEUSDT', 'BTCUSDT', "SOLUSDT", "ETHUSDT"];
+const viableTickers = ['DOGEUSDT', 'BTCUSDT', "SOLUSDT", "ETHUSDT", "XLMUSDT"];
 const buyPower = 1000;
 const positionsChannelId = process.env.POSITIONS_CHANNEL;
 
 async function addPosition(position) {
-    if (viableTickers.includes(position.ticker)) {
-        const tickerInfo = await getTickerInfo(position.ticker);
+    if (viableTickers.includes(position.symbol)) {
+        const tickerInfo = await getTickerInfo(position.symbol);
         const tickerMarketPrice = tickerInfo.list[0].markPrice;
-        const instrumentsInfo = await getInstrumentsInfo(position.ticker);
+        const instrumentsInfo = await getInstrumentsInfo(position.symbol);
         const tickSize = instrumentsInfo.list[0].lotSizeFilter.qtyStep;
         const qty = await calculateQuantity(tickerMarketPrice, tickSize);
         newPosition = {
-            tickerName: position.ticker,
+            tickerName: position.symbol,
             tickerMarketPrice: tickerMarketPrice,
             positionQuantity: qty,
             positionSide: position.side
         }
         positions.push(newPosition);
-        openBybitPosition(position.ticker, position.side, qty);
+        openBybitPosition(position.symbol, position.side, qty);
         sendMessage(positionsChannelId, formatUserMessage(position));
     }
 }
 
 async function closePosition(positionInfo) {
-    if (viableTickers.includes(positionInfo.ticker)) {
-        const positionToClose = positions.find(position => position.tickerName === positionInfo.ticker);
+    if (viableTickers.includes(positionInfo.symbol)) {
+        const positionToClose = positions.find(position => position.tickerName === positionInfo.symbol);
         if (positionToClose) {
             let side = positionToClose.positionSide == "Buy" ? "Sell" : "Buy";
             openBybitPosition(positionToClose.tickerName, side, positionToClose.positionQuantity);
             // The position exists, so you can now proceed to close it
             console.log(`Found position to close: `, positionToClose);    
-            sendMessage(positionsChannelId, formatUserMessage(positionToClose));
+            sendMessage(positionsChannelId, formatUserMessage(positionInfo));
         } else {
             console.log(`Position with ticker name ${positionInfo.ticker} not found.`);
         }
